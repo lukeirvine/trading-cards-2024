1cd56f0f4872e68c25ac2441c447ed99af429e19
lukeirvine <lukeirvinefreelance@gmail.com>
2024-06-24 23:50:10 -0700

    new csv with 2024 staff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from classes.card_app import CardApp
 
-app = CardApp("staff_input_one_each.csv", "output", generate_pdfs=True, use_print_layout=True)
+app = CardApp("staff-2024-final.csv", "output", generate_pdfs=True, use_print_layout=True)
 
 app.run()
