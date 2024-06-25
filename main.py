f3bab7dbdbe638eb7029e95d57345cb8beca8375
lukeirvine <lukeirvinefreelance@gmail.com>
2024-06-25 00:33:02 -0700

    fixed csv in google sheets

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from classes.card_app import CardApp
 
-app = CardApp("tom.csv", "output", generate_pdfs=True, use_print_layout=True)
+app = CardApp("staff_input_one.csv", "output", generate_pdfs=True, use_print_layout=True)
 
 app.run()
